e2a039a4f77e49bd043eb1bf950cde8b96788e7c
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-09 21:52:30 -0300

    feat(part1): courseinfo 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-	const course = "Desenvolvimento de aplicação Half Stack";
-	const parts = [
-    {
-      name: 'Fundamentos da biblioteca React',
-      exercises: 10
-    },
-    {
-      name: 'Usando props para passar dados',
-      exercises: 7
-    },
-    {
-      name: 'Estado de um componente',
-      exercises: 14
-    }
-  ]
+	const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
 
 	return (
 		<div>
-			<Header course={course}/>
-      <Content parts={parts}/>
-			<Total parts={parts} />
+			<Header course={course.name}/>
+      <Content parts={course.parts}/>
+			<Total parts={course.parts} />
 		</div>
 	);
 };
