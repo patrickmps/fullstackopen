b7cb1a82f33a18744b9f6e0b68f4a496df1d1901
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-13 18:33:09 -0300

    feat(part1): unicafe 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  // salve os cliques de cada botão em seu próprio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      Programe aqui!
+    </div>
+  )
+}
+
+export default App
