66f3b4dbe8bc867a393b23c320b1fc0f20861cf2
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-16 22:48:08 -0300

    feat(part2): phonebook 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "7399999-9999" }]);
+	const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
+	const [search, setSearch] = useState("");
+
+  const showPersons = search.trim().length === 0 ? persons : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
 
 	const addPhoneNumber = (event) => {
 		event.preventDefault();
 		const newPerson = {
+      id: persons.length + 1,
 			name: newName,
       number: newNumber
 		};
@@ -18,8 +27,8 @@ const App = () => {
 				JSON.stringify(newPerson).toLowerCase()
 		);
 
-		if (newPerson.name.trim().length === 0) {
-			window.alert(`The name is empty.`);
+		if (newPerson.name.trim().length === 0 || newPerson.number.trim().length === 0) {
+			window.alert(`The name or number is empty.`);
 		} else if (equalObjects.length > 0) {
 			window.alert(`${newPerson.name} is already added to phonebook.`);
 		} else {
@@ -34,10 +43,17 @@ const App = () => {
 	const handleNumberChange = (event) => {
 		setNewNumber(event.target.value);
 	};
+	const handleSearchChange = (event) => {
+		setSearch(event.target.value);
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
+      <div>
+					filter shown with: <input value={search} onChange={handleSearchChange} />
+				</div>
+      <h2>Add a new</h2>
 			<form onSubmit={addPhoneNumber}>
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
@@ -48,8 +64,8 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
-				<span key={person.name}>
+			{showPersons.map((person) => (
+				<span key={person.id}>
 					{person.name} {person.number}
 					<br />
 				</span>
