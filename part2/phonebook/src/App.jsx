33a208e338fbef1d0de6df3bb0a2c00dc1af1b91
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-16 22:28:01 -0300

    feat(part2): phonebook 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,40 +1,55 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [newName, setNewName] = useState("");
 
-  const addPhoneNumber = (event) => {
-    event.preventDefault()
-    const newPerson = {
-      name: newName
-    }
+	const addPhoneNumber = (event) => {
+		event.preventDefault();
+		const newPerson = {
+			name: newName,
+		};
 
-    setPersons(persons.concat(newPerson))
-    setNewName('')
-  }
+		const equalObjects = persons.filter(
+			(person) =>
+				JSON.stringify(person).toLowerCase() ===
+				JSON.stringify(newPerson).toLowerCase()
+		);
 
-  const  handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+		if (newPerson.name.trim().length === 0) {
+			window.alert(`The name is empty.`);
+		} else if (equalObjects.length > 0) {
+			window.alert(`${newPerson.name} is already added to phonebook.`);
+		} else {
+			setPersons(persons.concat(newPerson));
+			setNewName("");
+		}
+	};
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPhoneNumber}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <span key={person.name}>{person.name}<br/></span>)}
-    </div>
-  )
-}
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
 
-export default App
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addPhoneNumber}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<span key={person.name}>
+					{person.name}
+					<br />
+				</span>
+			))}
+		</div>
+	);
+};
+
+export default App;
