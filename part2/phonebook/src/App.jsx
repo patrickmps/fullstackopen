3f608053167afb8f28da605b479df0008d74f2b2
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-18 18:59:42 -0300

    feat(part2): phonebook 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [search, setSearch] = useState("");
@@ -58,6 +54,12 @@ const App = () => {
 	const handleSearchChange = (event) => {
 		setSearch(event.target.value);
 	};
+
+	useEffect(() => {
+		axios.get("http://localhost:3000/persons").then(response => {
+			setPersons(response.data)
+		})
+	}, [])
 
 	return (
 		<div>
