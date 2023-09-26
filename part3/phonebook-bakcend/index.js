53d044e689eaefebb5817405ceeff39794574906
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-09-25 22:03:22 -0300

    feat!: connect phonebook back-end with front-end
    The front end of the phonebook built in part2 was changed to use the backend being developed in part3
    
    BREAKING CHANGE: now a backend developed in part3 is being used, a proxy was created in vite.config.js to access the api

--- part3/phonebook-bakcend/index.js
+++ part3/phonebook-bakcend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 
 morgan.token('data', (req, res) => {
@@ -88,7 +89,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
