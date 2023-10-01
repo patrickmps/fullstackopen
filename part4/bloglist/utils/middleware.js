cd971d0a4346e572acf298c8560091c42e855cf8
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-10-01 16:42:12 -0300

    refactor(part4): structure the project 4.2

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,31 @@
+const { info, error } = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  info('Method:', request.method)
+  info('Path:  ', request.path)
+  info('Body:  ', request.body.title)
+  info('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
