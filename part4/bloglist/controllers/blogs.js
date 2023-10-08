d4e1ec9cec74b930b9dedddd76005a0a847be424
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-10-08 16:19:58 -0300

    feat(part4): adds get and delete functionality
    Adds the features of updating and deleting a blog, and creates tests for these features. Also adds tests for these features

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -26,4 +26,33 @@ blogsRouter.post('/', async (request, response) => {
   return response.status(201).json(result);
 });
 
+blogsRouter.get('/:id', async (request, response) => {
+  const id = request.params.id
+  const blog = await Blog.findById(id)
+
+  return response.status(200).json(blog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  await Blog.findByIdAndRemove(id)
+
+  return response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const result = await Blog.findByIdAndUpdate(id, blog, { new: true })
+
+  return response.status(200).json(result)
+})
+
 module.exports = blogsRouter;
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -15,68 +15,120 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 1000000);
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 1000000);
 
-test('checks if the id is with underscore', async () => {
-  const { body } = await api.get('/api/blogs');
-  body.forEach((blog) => expect(blog.id).toBeDefined());
-});
+  test('checks if the id is with underscore', async () => {
+    const { body } = await api.get('/api/blogs');
+    body.forEach((blog) => expect(blog.id).toBeDefined());
+  });
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-  };
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    const titles = response.body.map(b => b.title)
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    expect(titles).toContain(
+      'Go To Statement Considered Harmful'
+    )
+  })
+})
 
-  const titles = blogsAtEnd.map((b) => b.title);
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+    };
 
-  expect(titles).toContain(
-    'First class tests',
-  );
-});
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-test('blog without likes is not added', async () => {
-  const newBlog = {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-  };
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201);
+    const titles = blogsAtEnd.map((b) => b.title);
 
-  const blogsAtEnd = await helper.blogsInDb();
+    expect(titles).toContain(
+      'First class tests',
+    );
+  });
 
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-});
+  test('blog without likes is not added', async () => {
+    const newBlog = {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    };
 
-test('a blog can\'t be added without title or url', async () => {
-  const newBlog = {
-    title: '',
-    author: 'Robert C. Martin',
-    url: '',
-    likes: 2,
-  };
-  await api.post('/api/blogs').send(newBlog).expect(400);
-});
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  });
+
+  test('a blog can\'t be added without title or url', async () => {
+    const newBlog = {
+      title: '',
+      author: 'Robert C. Martin',
+      url: '',
+      likes: 2,
+    };
+    await api.post('/api/blogs').send(newBlog).expect(400);
+  });
+})
+
+describe('deletion of a blog', () => {
+  test('a valid blog can be deleted', async () => {
+    let blogsAtEnd = await helper.blogsInDb();
+    const blogsId = blogsAtEnd.map((b) => b.id);
+
+    const toBeDeleted = blogsId[0]
+
+    await api
+      .delete(`/api/blogs/${toBeDeleted}`)
+      .expect(204)
+
+    blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+    expect(blogsId).toContain(
+      toBeDeleted
+    );
+  });
+})
+
+describe('updating a blog', () => {
+  test('a valid blog can be updated', async () => {
+    const blog = {
+      title: 'Blog do Tulio',
+      author: 'Tulio Calil',
+      url: 'https://tuliocalil.com/',
+      likes: 8,
+    }
+
+    const blogsAtEnd = await helper.blogsInDb()
+    const toBeUpdated = blogsAtEnd[0]
+
+    const { body: result } = await api.put(`/api/blogs/${toBeUpdated.id}`).send(blog).expect(200);
+
+    expect({ title: result.title, author: result.author, url: result.url, likes: result.likes }).toEqual(blog)
+  });
+})
 
 afterAll(async () => {
   await mongoose.connection.close();
