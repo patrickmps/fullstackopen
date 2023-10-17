eac135afed7fe3ab0f736658bfcfc706145c6377
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-10-17 15:46:28 -0300

    feat(part5): adds authentication and blog creation
    adds authentication by storing user information in localStorage, also adds system logout option.
    Adds the form for adding a new blog.
    Add notifications as feedback to the user

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import loginService from "./services/login";
 import blogService from "./services/blogs";
+import { Notification } from "./components/Notification";
 
 const App = () => {
 	const [blogs, setBlogs] = useState([]);
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [user, setUser] = useState(null);
+	const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+	const [message, setMessage] = useState("");
 
 	const handleLogin = async (event) => {
 		event.preventDefault();
@@ -18,17 +21,52 @@ const App = () => {
 				password,
 			});
 
+			window.localStorage.setItem(
+				"loggedBlogListappUser",
+				JSON.stringify(user)
+			);
 			blogService.setToken(user.token);
 			setUser(user);
 			setUsername("");
 			setPassword("");
 		} catch (exception) {
-			console.log(exception);
-			// setErrorMessage("Wrong credentials");
-			// setTimeout(() => {
-			// 	setErrorMessage(null);
-			// }, 5000);
+			setMessage({ text: "Wrong credentials", success: false });
+			setTimeout(() => {
+				setMessage(null);
+			}, 5000);
 		}
+	};
+
+	const handleLogout = async () => {
+		window.localStorage.clear();
+		setUser(null);
+	};
+
+	const handleCreateBlog = async (event) => {
+		event.preventDefault();
+
+		try {
+			const response = await blogService.create(newBlog);
+			setBlogs(blogs.concat(response));
+			setNewBlog("");
+			setMessage({
+				text: `a new blog ${response.title} by ${response.author}`,
+				success: true,
+			});
+			setTimeout(() => {
+				setMessage(null);
+			}, 5000);
+		} catch (exception) {
+			setMessage({ text: exception.message, success: false });
+			setTimeout(() => {
+				setMessage(null);
+			}, 5000);
+		}
+	};
+
+	const handleBlogChange = (event) => {
+		console.log(newBlog);
+		setNewBlog({ ...newBlog, [event.target.name]: event.target.value });
 	};
 
 	const loginForm = () => (
@@ -38,7 +76,7 @@ const App = () => {
 				<input
 					type="text"
 					value={username}
-					name="Username"
+					name="username"
 					onChange={({ target }) => setUsername(target.value)}
 				/>
 			</div>
@@ -47,13 +85,55 @@ const App = () => {
 				<input
 					type="password"
 					value={password}
-					name="Password"
+					name="password"
 					onChange={({ target }) => setPassword(target.value)}
 				/>
 			</div>
 			<button type="submit">login</button>
 		</form>
 	);
+
+	const blogForm = () => (
+		<form onSubmit={handleCreateBlog}>
+			<div>
+				title
+				<input
+					type="text"
+					value={newBlog.title || ""}
+					name="title"
+					onChange={handleBlogChange}
+				/>
+			</div>
+			<div>
+				author
+				<input
+					type="text"
+					value={newBlog.author || ""}
+					name="author"
+					onChange={handleBlogChange}
+				/>
+			</div>
+			<div>
+				url
+				<input
+					type="text"
+					value={newBlog.url || ""}
+					name="url"
+					onChange={handleBlogChange}
+				/>
+			</div>
+			<button type="submit">create</button>
+		</form>
+	);
+
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem("loggedBlogListappUser");
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON);
+			setUser(user);
+			blogService.setToken(user.token);
+		}
+	}, []);
 
 	useEffect(() => {
 		blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -62,11 +142,15 @@ const App = () => {
 	return (
 		<div>
 			<h2>blogs</h2>
+			{message && <Notification message={message} />}
 			{user === null ? (
 				loginForm()
 			) : (
 				<>
-					<p>{user.name} logged in</p>
+					<p>
+						{user.name} logged in <button onClick={handleLogout}>logout</button>
+					</p>
+					{blogForm()}
 					{blogs.map((blog) => (
 						<Blog key={blog.id} blog={blog} />
 					))}
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,17 @@
+export const Notification = ({ message }) => {
+	if (message === null) {
+		return null;
+	}
+
+	const style = {
+		color: message.success ? "green" : "red",
+		background: "lightgrey",
+		fontSize: "20px",
+		borderStyle: "solid",
+		borderRadius: "5px",
+		padding: "10px",
+		marginBottom: "10px",
+	};
+
+	return <div style={style}>{message.text}</div>;
+};
