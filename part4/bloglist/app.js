9dd2053b3768d1bcf542efc116e87200e0312219
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2023-11-02 12:03:08 -0300

    feat(part6): add unicafe-redux
    add unicafe using redux for state management

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const app = express();
 const cors = require('cors');
 const config = require('./utils/config');
 const { info } = require('./utils/logger');
 require('express-async-errors');
 const middleware = require('./utils/middleware');
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const blogsRouter = require('./controllers/blogs');
-
 
 mongoose.set('strictQuery', false);
 
@@ -29,14 +29,14 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
-
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', middleware.userExtractor, blogsRouter);
 
 if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
+  // eslint-disable-next-line global-require
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
 }
 
 app.use(middleware.unknownEndpoint);
