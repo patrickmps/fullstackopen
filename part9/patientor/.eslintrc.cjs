f9c331b5288cb9f6246e85012b8546dad3c2d729
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2024-01-27 20:18:17 -0300

    feat(part9): add patientor frontend

---
+++ part9/patientor/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/semi": ["error"],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { "argsIgnorePattern": "^_" }
+    ],
+    "no-case-declarations": "off"
+  },
+}
---
+++ part9/patientor/README.md
@@ -0,0 +1,5 @@
+# Patientor frontend
+
+### Getting started
+  - To get the app running just install its dependencies with ```npm install``` and run it with ```npm run dev```.
+  - The app should work without a backend, but make sure that the request made to ```/api/ping``` made on startup is successful before continuing.
